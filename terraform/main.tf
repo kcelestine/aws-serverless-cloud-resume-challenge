e0a59d57b718c62a9765060330e8458cd3cb7be4
Khadijah <khadijah.celestine@gmail.com>
2023-04-05 16:08:17 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,7 +90,6 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-  policy = data.aws_iam_policy_document.dynamodb_access.json
 }
 
 data "archive_file" "lambda_file" {
